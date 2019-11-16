5da18d573b140293279e9c7af0005e0784e61b2e
Conary36 <cbeckford144@gmail.com>
2019-11-16 12:11:55 -0500

    troubleshooting all files with no results

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import NumberButton from './components/NumberButtons/NumberButton';
+import Numbers from './components/NumberButtons/Numbers';
+import Operators from './components/OperatorButtons/Operators';
+import Specials from './components/SpecialButtons/Specials';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -18,6 +22,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <NumberButton/>
+        <Numbers/>
+        <Operators/>
+        <Specials/>
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ const NumberButton = (props) => {
   return (
     <button
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-        className="number_button"
+  
         onClick={() => props.setNumberState(props.button)}
         >
       <span>{props.button.value}</span>
